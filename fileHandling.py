2b3c755bce05264f2a185ab39959d145d3cdef1d
Justwait <Justw8@users.noreply.github.com>
2018-06-10 16:41:11 +0200

    Update fileHandling.py

--- fileHandling.py
+++ fileHandling.py
@@ -1,32 +1,32 @@
-fileName = 'test.txt'
-linebreak = '\n'
+fileName = 'test.txt' # Het bestand waarin word geschreven
+linebreak = '\n' # enter in een variable zetten voor duidelijkheid
 
-def getItemsFromFile():
+def getItemsFromFile(): # Bestand Uitlees functie
     try:
-       file = open(fileName, "r")
+       file = open(fileName, "r") # proberen het bestand te openen met als regel: r - Read
     except:
-        return []
+        return [] # Als het niet lukt, leeg array terug geven aan de functie uitkomst
 
-    listOfItems = []
-    for item in file:
-        listOfItems.append(item.strip(linebreak).split(', '))
+    listOfItems = [] # Array aanmaken voor de gevonden gegevens
+    for item in file: # Door elke regel van het bestand heen loopen
+        listOfItems.append(item.strip(linebreak).split(', ')) # linebreak weghalen, en aan het einde van de array toevoegen.
 
-    file.close()
-    return listOfItems
+    file.close() # bestand afsluiten
+    return listOfItems # Array met gelezen regels/items terug geven aan de functie uitkomst
 
-def writeItemsToFile(listOfItems):
+def writeItemsToFile(listOfItems):  # Bestand Schrijf functie
     try:
-        file = open(fileName, "w")
+        file = open(fileName, "w") # proberen het bestand te openen met als regel: w - Write
     except:
-        return False
+        return False # Lukt het niet dan False teruggeven aan de functie uitkomst
 
-    for item in listOfItems:
-        file.write((', '.join(map(str, item))) + linebreak)
+    for item in listOfItems: # Door alle items loopen die aan deze functe zijn meegegeven
+        file.write((', '.join(map(str, item))) + linebreak) # Van ieder item een string maken, en een linebreak achter het item plaatsen 
 
-    file.close()
-    return True
+    file.close() # bestand afsluiten
+    return True # True teruggeven aan de functie, omdat het is uitgevoerd
 
-items = [['a,99,22'], ['b,34,dd'], ['c,5,21']]
-writeItemsToFile(items)
-items = getItemsFromFile()
-print(items)
+items = [['a,99,22'], ['b,34,dd'], ['c,5,21']] # Array met items
+writeItemsToFile(items) # Array met items in het bestand schrijven (roept de functie hierboven benoemd aan)
+items = getItemsFromFile() # items uitlezen met de bovenstaande functie en in de items variable zetten
+print(items) # items printen
